4a54d3445fbfd908abfdf1907a45cc95eb929578
Ferozkan05 <151832688+Ferozkan05@users.noreply.github.com>
2025-04-25 21:17:57 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -46,7 +46,7 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.ecs.arn
+    target_group_arn = aws_lb_target_group.admissions.arn
   }
 }
 resource "aws_lb_listener_rule" "patient" {
